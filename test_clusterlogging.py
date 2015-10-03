7bb7fce8e8ede786837ea77c5e14343b195315d4
Sergey Romanov <xxsmotur@gmail.com>
2015-10-03 23:50:41 +0500

    Add test

--- test_clusterlogging.py
+++ test_clusterlogging.py
@@ -38,6 +38,15 @@ class TestBasic(unittest.TestCase):
         logg.debug(1, "A")
         self.assertEqual(len(logg.clustering()), 5)
 
+    def test_empty_clusters(self):
+        logg = clusterlogging.ClusterLogging()
+        try:
+            logg.clustering()
+        except:
+            self.assertTrue(True)
+            return
+        self.assertTrue(False)
+
 
 if __name__ == '__main__':
     unittest.main()
